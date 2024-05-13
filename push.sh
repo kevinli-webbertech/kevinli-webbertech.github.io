7e7f654df507411344d181963d5122786df9a9fa
kevin li <kevin.li@webbertech.com>
2024-05-12 21:15:47 -0400

    updating code

--- push.sh
+++ push.sh
@@ -5,7 +5,7 @@ echo $(pwd)
 export project_root="/home/xiaofengli/git/kevinli-webbertech.github.io"
 echo $project_root
 cd $project_root
-git pull --no-ff
+git pull --no-ff --no-edit
 git add *
 git commit -m "updating code"
 git push
