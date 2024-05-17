7107b5b271cd5b0b7c6abf14012ddd8f3ec648cc
xiaofengli <xlics05@gmail.com>
2024-05-16 20:14:38 -0400

    updating code

--- push.sh
+++ push.sh
@@ -1,10 +1,9 @@
 echo "commiting code to github..."
-echo $(pwd)
-# TODO figure out project root
 
-export project_root="/home/xiaofengli/git/kevinli-webbertech.github.io"
+project_root=$(pwd)
 echo $project_root
 cd $project_root
+git pull --no-ff
 git add *
 git commit -m "updating code"
 git push
