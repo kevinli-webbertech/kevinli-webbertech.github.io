f87fc52c21312deb2f1184f02c9a8180d990daea
kevin li <kevin.li@webbertech.com>
2024-06-12 21:24:59 -0400

    partial work

--- blog/create_blog.sh
+++ blog/create_blog.sh
@@ -2,33 +2,59 @@
 
 mdPath="md"
 htmlPath="html"
-mdFileName=$1
+mdDir=$1
+
 
 if [[ -z $1 ]];then
-  echo "pleaes provide file name in .md format"
+  echo "please provide a file in .md format"
   exit 1
 fi
 
-mdFilePath=${mdPath}/${mdFileName}
-echo "md file path is: ${mdFilePath}"
+getHtmlDir() {
+  
+}
 
-echo "mdFileName: ${mdFileName}"
+getAllMarkdownfiles() {
+  files=$(ls $mdDir/*.md)
+  echo $files
+}
 
-# GET MD FILE
-mdFile=`echo ${mdFileName} |rev | cut -d '/' -f 1|rev`
+getMDWithoutHtml() {
+  mdFiles=$(getAllMarkdownfiles)
+  for file in $mdFiles; do
+      htmlPath=$(getHTMLFilePath $file)
+      echo "****"
+      echo $htmlPath
+      echo "\n"
+  done
+}
 
-# GET HTML FILE
-htmlFileName=`sed -E s/"md"/"html"/ <<< ${mdFile}`
-echo "html file path is: ${htmlPath}/${htmlFileName}"
+getHTMLFilePath() {
+  mdFilePath=$1
+  echo -e "$mdFilePath\n"
+  # GET MD FILE
+  mdFile=`echo ${mdFilePath} |rev | cut -d '/' -f 1|rev`
 
-# COPY HTML FILE
-cp ${htmlPath}/template.html $htmlPath/${htmlFileName}
+  # GET HTML FILE
+  htmlFileName=`sed -E s/"md"/"html"/ <<< ${mdFile}`
+  echo -e "html file path is: ${htmlPath}/${htmlFileName}\n"
+}
 
-# SED TEMPLATE
-echo "debugging"
-echo ${mdFileName}  
-echo ${htmlPath}/${htmlFileName}
-#sed -i s/"file.md"/"${mdFileName}"/g ${htmlPath}/${htmlFileName}
+  # COPY HTML FILE
+ # cp ${htmlPath}/template.html $htmlPath/${htmlFileName}
 
-#https://stackoverflow.com/questions/13210880/replace-one-substring-for-another-string-in-shell-script
-#echo `sed s/.md/.html/ $fileName`
+  # SED TEMPLATE
+  #echo "debugging"
+  #echo ${mdFileName}  
+  #echo ${htmlPath}/${htmlFileName}
+
+  #sed -i s/"file.md"/"${mdFileName}"/g ${htmlPath}/${htmlFileName}
+  #https://stackoverflow.com/questions/13210880/replace-one-substring-for-another-string-in-shell-script
+  #echo `sed s/.md/.html/ $fileName`
+
+
+#mdFiles=$(getAllMarkdownfiles)
+echo "testing...."
+#echo $mdFiles
+getMDWithoutHtml
+#create_html
