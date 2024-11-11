b3476e9ad77314426ad36d1bfdf8c5a61b97f046
Kevin Li <xiaofeng.li2@va.gov>
2024-11-11 10:12:10 -0500

    improve_the_git_shell

--- push.sh
+++ push.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+echo "$1"
+echo $#
+if [ "$#" -ne 1 ] ; then
+  echo "Usage: $0 commit_message" >&2
+  exit 1
+fi
+
+message="$1"
 echo "commiting code to github..."
 
 project_root=$(pwd)
@@ -6,7 +15,7 @@ cd $project_root
 git pull --no-ff --no-edit
 git add -A .
 #git add -A ..
-git commit -m "updating code"
+git commit -m $message
 git push
 
 echo "pushing code to github..."
