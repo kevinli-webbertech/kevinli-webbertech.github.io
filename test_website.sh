bf5ccec6360690451f3da0f20167597fc1557d3e
Kevin Li <xiaofeng.li2@va.gov>
2025-02-22 20:47:45 -0500

    fix the test script

--- test_website.sh
+++ test_website.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# TODO need to fix this file
+
 #URL="$1"
 URL="https://kevinli-webbertech.github.io/blog.html"
 
@@ -11,7 +13,7 @@ fi
 check_link() {
   local link="$1"
   status_code=$(curl -s -o /dev/null -w "%{http_code}" "$link")
-
+  echo $status_code
   if [[ "$status_code" -ge 400 ]]; then
     echo "Broken link: $link (Status code: $status_code)"
   elif [[ "$status_code" -ge 300 && "$status_code" -lt 400 ]]; then
