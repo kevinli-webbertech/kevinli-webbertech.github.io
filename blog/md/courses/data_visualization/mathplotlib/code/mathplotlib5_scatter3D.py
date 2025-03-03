42c923bf5f1916759abeda448cd22370ab339cab
Kevin Li <xiaofeng.li2@va.gov>
2025-03-02 19:22:51 -0500

    update blog.html

---
+++ blog/md/courses/data_visualization/mathplotlib/code/mathplotlib5_scatter3D.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import numpy as np
+# https://matplotlib.org/2.2.2/mpl_toolkits/index.html
+# python3 -m pip install basemap
+ax = plt.axes(projection='3d')
+zline = np.linspace(0, 15, 1000)
+xline = np.sin(zline)
+yline = np.cos(zline)
+ax.plot3D(xline, yline, zline, 'gray')
+# Data for three-dimensional scattered points
+zdata = 15 * np.random.random(100)
+xdata = np.sin(zdata) + 0.1 * np.random.randn(100)
+ydata = np.cos(zdata) + 0.1 * np.random.randn(100)
+ax.scatter3D(xdata, ydata, zdata, c=zdata, cmap='Greens');
+
+
+plt.show()
